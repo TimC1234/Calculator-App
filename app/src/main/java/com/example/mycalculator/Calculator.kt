8ac594fc3c4dc4c705abca71ebd1aee4d5dce399
TimC1234 <timchou6252@gmail.com>
2025-05-19 15:47:38 -0700

    Created title of the app

--- app/src/main/java/com/example/mycalculator/Calculator.kt
+++ app/src/main/java/com/example/mycalculator/Calculator.kt
@@ -1,5 +1,27 @@
 package com.example.mycalculator
 
-public fun calc() {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
+@Composable
+public fun calc() {
+//    Text("Hello")
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        // Here all the UI elements will be stacked below each other
+        Text("Calculator App", style = MaterialTheme.typography.headlineLarge)
+
+
+    }
 }
