82ceb89cde054b593e4ddff3e68dcca9269a004a
TimC1234 <timchou6252@gmail.com>
2025-05-19 17:09:54 -0700

    Started adding logic for button presses

--- app/src/main/java/com/example/mycalculator/Calculator.kt
+++ app/src/main/java/com/example/mycalculator/Calculator.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -12,87 +13,90 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun calc() {
 
-    //Spacer(modifier = Modifier.width(20.dp))
+    var input = ""
+
     Column(
         modifier = Modifier.fillMaxSize(),
     ) {
 
         // Here all the UI elements will be stacked below each other
         //Text("Calculator App", style = MaterialTheme.typography.headlineLarge)
-
+        Spacer(modifier = Modifier.height(200.dp))
         Row() {
-            Button(onClick = {}) {
+            Button(onClick = { input += 7 }) {
                 Text("7")
             }
-            Button(onClick = {}) {
+            Button(onClick = { input += 8 }) {
                 Text("8")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input += 9}) {
                 Text("9")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input = input.dropLast(1)}) {
                 Text("Del")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input = ""}) {
                 Text("Clear")
             }
 
         }
         Row() {
-            Button(onClick = {}) {
+            Button(onClick = {input += 4}) {
                 Text("4")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input += 5}) {
                 Text("5")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input += 6}) {
                 Text("6")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input += "x"}) {
                 Text("x")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input += "/"}) {
                 Text("/")
             }
         }
         Row() {
-            Button(onClick = {}) {
+            Button(onClick = {input += 1}) {
                 Text("1")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input += 2}) {
                 Text("2")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input += 3}) {
                 Text("3")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input += "+"}) {
                 Text("+")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input += "-"}) {
                 Text("-")
             }
         }
 
         Row() {
-            Button(onClick = {}) {
+            Button(onClick = {input += 0}) {
                 Text("0")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input += "."}) {
                 Text(".")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input += "="}) {
                 Text("=")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input += "("}) {
                 Text("(")
             }
-            Button(onClick = {}) {
+            Button(onClick = {input += ")"}) {
                 Text(")")
+                
             }
         }
 
